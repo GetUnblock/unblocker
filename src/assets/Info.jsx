e285df9236da9f2f440f8e8abc55ce549b9f8440
filipegoncalvesBP <filipe.goncalves@bytepitch.com>
2023-03-08 11:42:10 +0000

    removed unused icons and fix Info svg

--- src/assets/Info.jsx
+++ src/assets/Info.jsx
@@ -1,10 +1,10 @@
 export const Info = () => {
     return (
         <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-            <g clip-path="url(#clip0_6404_173766)">
-                <path d="M17.25 9C17.25 13.5563 13.5563 17.25 9 17.25C4.44365 17.25 0.75 13.5563 0.75 9C0.75 4.44365 4.44365 0.75 9 0.75C13.5563 0.75 17.25 4.44365 17.25 9Z" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M9 12.6V9" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M9 5.3999H9.009" stroke="black" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" />
+            <g clipPath="url(#clip0_6404_173766)">
+                <path d="M17.25 9C17.25 13.5563 13.5563 17.25 9 17.25C4.44365 17.25 0.75 13.5563 0.75 9C0.75 4.44365 4.44365 0.75 9 0.75C13.5563 0.75 17.25 4.44365 17.25 9Z" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M9 12.6V9" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+                <path d="M9 5.3999H9.009" stroke="black" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
             </g>
             <defs>
                 <clipPath id="clip0_6404_173766">
