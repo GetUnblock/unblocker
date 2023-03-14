5b78a6be82bd1087e729a235df8c3bc5b1cb45e4
filipegoncalvesBP <filipe.goncalves@bytepitch.com>
2023-03-14 09:56:57 +0000

    Added check for metamask installed and not found page

--- src/index.tsx
+++ src/index.tsx
@@ -2,14 +2,19 @@ import * as ReactDOM from 'react-dom/client';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import App from './App';
+import NotFound from './NotFound';
 import theme from './theme';
 
 const rootElement = document.getElementById('root');
 const root = ReactDOM.createRoot(rootElement!);
+const ethereum = window.ethereum;
+const isMetaMask = window.ethereum ? window.ethereum.isMetaMask : false;
 
 root.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
-    <App />
+    {(ethereum && isMetaMask) ?
+      <App /> : <NotFound />
+    }
   </ThemeProvider>,
 );
